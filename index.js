399d339a669257c7bdc9f6ac77a2664780d64352
Brajesh-S <Brajesh-S@users.noreply.github.com>
2024-03-15 15:08:17 +0530

    NitPicks

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ mongoose
         
         app.use(express.static(path.join(__dirname, "./front-end/movies-wiki/build")))
         app.get("/", (req, res)=>{
-            res.sendFile(path.join(__dirname, "/front-end/movies-wiki/build/index.html"))
+            res.sendFile(path.join(__dirname, "./front-end/movies-wiki/build/index.html"))
         });
        
                                                                   
